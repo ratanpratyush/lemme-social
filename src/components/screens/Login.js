a2aa9f38a483b3f4ff42f371921eef21ce50db25
ratanpratyush <ratan.pratyush@gmail.com>
2020-05-01 21:31:30 +0530

    Login and Signup Page is created.

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import './Login.css';
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <div id="parent">
+            <h1 align="center" style={{fontFamily:"TimesNewRoman"}}>Login</h1>
+            <div>
+            <form action="/action_page.php" method="get" onSubmit= {alert("You are submitting this Login Form" )}> 
+                <div class="imgcontainer">
+                    <img src={require('./img_avatar2.png')} alt="Avatar" class="avatar" align="center"/>
+                </div>
+
+                <div class="container">
+                    <label for="Email"><b>Email</b></label>
+                    <input type="email" placeholder="Enter Email" name="Email" required/>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required/>
+        
+                    <button align="center" type="submit">Login</button><br />
+                </div>
+            </form>
+
+            </div>
+        </div>
+        
     );
 }
 
--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 const SignUp = () => {
     return (
-        <h1>Signup</h1>
+        <div>
+            <h1 align="center" style={{fontFamily:"TimesNewRoman"}}>Signup</h1>
+            <div>
+                <form action="/action_page.php" method="post" onSubmitCapture = {alert("The account is created.")}>
+                    <div className="container">
+                        <p>Please fill this form to create an account.</p>
+                        <hr />
+
+                        <label for="FName"><b>First Name</b></label>
+                        <input type="text" placeholder="Enter First Name" name="FName" required />
+
+                        <label for="LName"><b>Last Name</b></label>
+                        <input type="text" placeholder="Enter Last Name" name="LName" required />
+
+                        <label for="email"><b>Email</b></label>
+                        <input type="email" placeholder="Enter Email" name="email" required />
+
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required />
+
+                        <label for="psw-repeat"><b>Repeat Password</b></label>
+                        <input type="password" placeholder="Repeat Password" name="psw-repeat" required />
+
+                        <div className="clearfix">
+                        <button type="submit" className="signupbtn" >Sign Up</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div> 
     );
 }
 
